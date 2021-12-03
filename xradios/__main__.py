e0d4de7e05774db7c157955f2f9d582dec0c3b41
André P. Santos <andreztz@gmail.com>
2021-12-03 01:44:52 -0300

    Add method to shutdown the server
    - Update Readme

--- xradios/__main__.py
+++ xradios/__main__.py
@@ -1,3 +1,10 @@
+import os 
+import subprocess
+import signal
+import time
+
+import psutil
+
 from xradios.tui import TUI
 from xradios.cli import parser
 from xradios.tui.services import initialize as initialize_services
@@ -10,28 +17,48 @@ from pyradios import RadioBrowser
 rb = RadioBrowser()
 
 
-def main():
+def process_is_running(cmd):
+    return any(
+        process for process in psutil.process_iter()
+        if cmd.lower() in process.name().lower()
+        )
 
+def kill_server(pid):
+    os.kill(pid, signal.SIGTERM)
+
+
+def main():
     cli = parser.parse_args()
     query = {}
+
+    server = "xradiosd"
+
+    subprocess.Popen(
+        [server],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+    time.sleep(5)
 
     if cli.stations_by_tag:
         query['command'] = "bytag"
         query["term"] = cli.stations_by_tag
-        
     else:
         query['command'] = "bytag"
         query["term"] = "trance"
 
     response = proxy.remote_search(**query)
+
     tui = TUI()
     tui.initialize(response)
     initialize_services()
     tui.run()
 
-
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception:
-        log.exception()
+    if process_is_running(server):
+        pid = None
+        for process in psutil.process_iter():
+            if server in process.name():
+                pid = int(process.pid)
+        if pid:
+            kill_server(pid)
