aef2384e27a43bac0d63f3c1723c1232f8e3b95d
André P. Santos <andreztz@gmail.com>
2022-01-11 01:37:05 -0300

    Remove unused methods

--- xradios/tui/buffers/listview.py
+++ xradios/tui/buffers/listview.py
@@ -19,33 +19,6 @@ class ListViewBuffer(Buffer):
             name=LISTVIEW_BUFFER,
         )
 
-    def line(self, index=None):
-        if index and index.isnumeric():
-            index = int(index) - 1
-            line = self._get_line(index)
-            return index, line
-        # i (index) is -> (int) line number
-        # text is -> (str) line content
-        index = self.document.cursor_position_row
-        text = self.document.current_line
-        return index, text
-
-    def _get_line(self, i):
-        try:
-            line = self.document.text.split("\n")[i]
-        except IndexError:
-            log.exception("ListViewBuffer._get_line(i)")
-        else:
-            return line
-
-    def get_index(self, **kwargs):
-        variables = kwargs.get("variables", None)
-        index = variables.get("subcommand") if variables else None
-
-        if index and index.isnumeric():
-            return self.line(index)[0]
-        return self.line()[0]
-
     def update(self, content):
         self.reset(Document(content, 0))
 
