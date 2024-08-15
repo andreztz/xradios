ea6910d9b1f2f0983b20571529984c799792829a
André P. Santos <andreztz@gmail.com>
2024-08-14 21:41:39 -0300

    refactor: Replace xmlrpc with jsonrpclib in server and client

--- xradios/tui/client.py
+++ xradios/tui/client.py
@@ -1,14 +1,3 @@
-from xmlrpc.client import ServerProxy
+from jsonrpclib import Server
 
-
-class Proxy:
-    def __init__(self, url):
-        self._xmlrpc_server_proxy = ServerProxy(url)
-    def __getattr__(self, name):
-        call_proxy = getattr(self._xmlrpc_server_proxy, name)
-        def _call(*args, **kwargs):
-            return call_proxy(args, kwargs)
-        return _call
-
-
-proxy = Proxy('http://localhost:10000')
+proxy = Server("http://localhost:10000")
