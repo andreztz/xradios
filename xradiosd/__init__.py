3d87c7e26bbc30bace8387980775983c73d2b99b
André P. Santos <andreztz@gmail.com>
2025-05-10 21:55:28 -0300

    Add CLI for xradiosd module
    Adds a command-line interface (CLI) to the xradiosd module.

--- xradiosd/__init__.py
+++ xradiosd/__init__.py
@@ -1,9 +1,13 @@
 import logging
 import os
+import sys
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
+import click
 from appdirs import user_config_dir, user_data_dir, user_log_dir
+
+from xradiosd.__about__ import __version__
 
 app_name = "xradios"
 
@@ -36,4 +40,15 @@ handler = RotatingFileHandler(
 logging.getLogger("xradiosd").addHandler(handler)
 logging.getLogger("xradios").addHandler(handler)
 
-# from xradiosd.server import main
+
+@click.command()
+@click.version_option(version=__version__, prog_name="xradiosd")
+@click.option("--host", default="127.0.0.1")
+@click.option("--port", default=10000, type=int)
+def main(host, port):
+    from xradiosd.server import run
+
+    try:
+        run(host, port)
+    except KeyboardInterrupt:
+        sys.exit(0)
